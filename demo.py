d86f674e05fd08049cab5d470e7104d05e3084ac
Simone Azeglio <simone.azeglio@gmail.com>
2020-08-29 13:50:52 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,6 +17,8 @@ from modules.keypoint_detector import KPDetector
 from animate import normalize_kp
 from scipy.spatial import ConvexHull
 
+from mhmovie import code
+
 
 if sys.version_info[0] < 3:
     raise Exception("You must use Python 3 or higher. Recommended version is Python 3.7")
@@ -153,5 +155,11 @@ if __name__ == "__main__":
         predictions = predictions_backward[::-1] + predictions_forward[1:]
     else:
         predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
+        
     imageio.mimsave(opt.result_video, [img_as_ubyte(frame) for frame in predictions], fps=fps)
+    m = code.movie(opt.result_video)
+    mu = code.music(driving_video)
+    mu.Aconvert()#convert wav to mp3
+    final = m+mu
+    final.save(opt.result_video)
 
